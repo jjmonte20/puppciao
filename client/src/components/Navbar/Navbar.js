05b544fc898851fe474894da799f60c215221f72
jjmonte20 <jjmonte20@gmail.com>
2019-08-26 11:21:02 -0500

    added menu to navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { withStyles } from '@material-ui/styles';
-import { Grid, AppBar, Toolbar, Button, IconButton, Typography } from '@material-ui/core';
+import { Grid, AppBar, Toolbar, Button, IconButton, Menu, MenuItem } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import "./styles.css";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,8 +17,22 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-
 class Navbar extends Component {
+    state = {
+      anchorEl: null
+    }
+
+    handleClick = e => {
+      this.setState({
+        anchorEl: true
+      })
+    }
+    
+    handleClose = () => {
+      this.setState({
+        anchorEl: false
+      })
+    }
 
     render() {
         return (
@@ -28,9 +43,28 @@ class Navbar extends Component {
                   justify="space-between"
                   container
                   spacing={24}>
-                <IconButton edge="start" className={useStyles.menuButton} color="inherit" aria-label="menu">
+                <IconButton 
+                  edge="start" 
+                  className={useStyles.menuButton} 
+                  color="inherit" 
+                  aria-label="menu"
+                  aria-controls="simple-menu"
+                  aria-haspopup="true"
+                  onClick={this.handleClick}
+                  >
                   <MenuIcon />
                 </IconButton>
+                <Menu
+                  id="simple-menu"
+                  anchorEl={this.state.anchorEl}
+                  keepMounted
+                  open={this.state.anchorEl}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem>Profile</MenuItem>
+                  <MenuItem>My Account</MenuItem>
+                  <MenuItem>Logout</MenuItem>
+                </Menu>
                 <Button color="inherit">Login</Button>
                 </Grid>
               </Toolbar>
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,16 +1,5 @@
 import axios from "axios";
 
 export default {
-  createUser: function(userData) {
-    return axios.post("/api/users", userData);
-  },
-  findMe: function() {
-    return axios.get("/api/users");
-  },
-  loginUser: function(loginUser) {
-    return axios.post("/api/log", loginUser);
-  },
-  logoutUser: function() {
-    return axios.get("/api/log");
-  }
+ 
 };
